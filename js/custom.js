f2c29146307f1e37e8bf2146ab2ab6406b522c23
siyi66400221 <66400221@qq.com>
2020-09-26 12:16:28 +0800

    Site updated: 2020-09-26 12:16:27

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,1 @@
-custom_css:
- - //https://cdn.jsdelivr.net/gh/sviptzk/HexoStaticFile@latest/Hexo/js/mouse_snow.min.js
+<script src='https://cdn.jsdelivr.net/gh/sviptzk/HexoStaticFile@latest/Hexo/js/mouse_snow.min.js'></script>
